1c9fe070b9cace478162d65aed5a5cc3c94562c9
shirotamaki <shiro.tamaki63@gmail.com>
2022-05-18 22:04:31 +0900

    Add: ドットインストール

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  var vm = new Vue ({
+    el: '#app',
+    data: {
+      newItem: '',
+      todos: [
+        'task1',
+        'task2',
+        'task3',
+      ]
+    },
+    methods: {
+      addItem: function() {
+        this.todos.push(this.newItem)
+        this.newItem = ''
+      },
+      deleteItem: function(index) {
+        if (confirm('are you sure?')) {
+          this.todos.splice(index, 1)
+        }
+      }
+    }
+  })
+})();
