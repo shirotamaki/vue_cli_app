e046928d5f86eec07b3013171442b63947d35657
shirotamaki <shiro.tamaki63@gmail.com>
2022-05-20 19:05:38 +0900

    Add: Purgeボタン（削除ボタン）

--- js/main.js
+++ js/main.js
@@ -17,27 +17,32 @@
       }]
     },
     methods: {
-      addItem: function() {
+      addItem: function () {
         var item = {
           title: this.newItem,
-          isDone: false
-        };
-        this.todos.push(item);
-        this.newItem = '';
+          isDone: false,
+        }
+        this.todos.push(item)
+        this.newItem = ''
       },
-      deleteItem: function(index) {
+      deleteItem: function (index) {
         if (confirm('are you sure?')) {
           this.todos.splice(index, 1)
         }
-      }
+      },
+      purge: function (index) {
+        if (!confirm('delete finished?')) {
+          return
+        }
+        this.todos = this.remaining
+      },
     },
     computed: {
-      remaining: function() {
-        var items = this.todos.filter(function(todo) {
-          return !todo.isDone;
+      remaining: function () {
+        return this.todos.filter(function (todo) {
+          return !todo.isDone
         })
-        return items.length
-      }
-    }
+      },
+    },
   })
 })();
