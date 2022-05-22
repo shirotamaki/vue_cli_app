4a349312bb41f660afd4396c91fca91a054b0e2b
shirotamaki <shiro.tamaki63@gmail.com>
2022-05-22 18:18:15 +0900

    Add: LocalStorageに保存したデータを読み出す方法、mountedを使う

--- js/main.js
+++ js/main.js
@@ -5,25 +5,18 @@
     el: '#app',
     data: {
       newItem: '',
-      todos: [{
-        title: 'task 1',
-        isDone: false
-      }, {
-        title: 'task 2',
-        isDone: false
-      }, {
-        title: 'task 3',
-        isDone: true
-      }]
+      todos: []
     },
       watch: {
         todos: {
           handler: function () {
-            localStorage.setItem('todos', JSON.stringify(this.todos))
-            alert('Data saved!')
+            localStorage.setItem('todos', JSON.stringify(this.todos)) ;
           },
-        deep: true,
+        deep: true
       }
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods: {
       addItem: function () {
