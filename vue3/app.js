59456dc06a886e01f0d3b87f9e6f95406f6bd195
shirotamaki <shiro.tamaki63@gmail.com>
2022-06-01 22:22:22 +0900

    Add: v-bind, v-on

--- vue3/app.js
+++ vue3/app.js
@@ -1,74 +1,107 @@
-const EventHandling = {
-  data() {
+const app = {
+  data () {
     return {
-      message: 'Hello Vue.js'
+      n: 0,
+      url: 'https://www.google.com/',
+      searchText: '',
+      myName: 'MouMou',
+      phoneNumber: ''
     }
   },
   methods: {
-    reverseMessage() {
-      this.message = this.message
-        .split('')
-        .reverse()
-        .join('')
-    }
-  }
-}
-
-const TwoWayBinding = {
-  data() {
-    return {
-      message: 'Hello Vue Vue!'
-    }
-  }
-}
-
-const ConditionalRendering = {
-  data() {
-    return {
-      seen: false
-    }
-  }
-}
-
-const ListRendering = {
-  data() {
-    return {
-      todos: [
-        { text: 'Learn JavaScript'},
-        { text: 'Learn Vue'},
-        { text: 'Rocknroll'}
-      ]
-    }
-  }
-}
-
-const TodoItem = {
-  props: ['todo'],
-  template: '<li>{{ todo.text }}</li>'
-}
-
-const TodoList = {
-  data() {
-    return {
-      groceryList: [
-        { id: 0, text: 'Vegetables' },
-        { id: 1, text: 'Cheese' },
-        { id: 2, text: 'Whatever else humans are supposed to eat' }
-      ]
-    }
+    countUp: function (comment) {
+      this.n += 1
+      alert(comment)
+    },
   },
-  components: {
-    TodoItem
-  }
+  methods: {
+    inputEvent ($event) {
+      console.log($event.target.value)
+    },
+  },
+  methods: {
+    changeMyName (event) {
+      this.myName = event.target.value
+    },
+  },
 }
 
-
-const app = Vue.createApp(TodoList)
-
-app.mount('#todo-list-app')
+Vue.createApp(app).mount('#app')
 
 
-Vue.createApp(EventHandling).mount('#event-handling')
-Vue.createApp(TwoWayBinding).mount('#two-way-binding')
-Vue.createApp(ConditionalRendering).mount('#conditional-rendering')
-Vue.createApp(ListRendering).mount('#list-rendering')
+//
+//
+// const EventHandling = {
+//   data() {
+//     return {
+//       message: 'Hello Vue.js'
+//     }
+//   },
+//   methods: {
+//     reverseMessage() {
+//       this.message = this.message
+//         .split('')
+//         .reverse()
+//         .join('')
+//     }
+//   }
+// }
+//
+// const TwoWayBinding = {
+//   data() {
+//     return {
+//       message: 'Hello Vue Vue!'
+//     }
+//   }
+// }
+//
+// const ConditionalRendering = {
+//   data() {
+//     return {
+//       seen: false
+//     }
+//   }
+// }
+//
+// const ListRendering = {
+//   data() {
+//     return {
+//       todos: [
+//         { text: 'Learn JavaScript'},
+//         { text: 'Learn Vue'},
+//         { text: 'Rocknroll'}
+//       ]
+//     }
+//   }
+// }
+//
+// const TodoItem = {
+//   props: ['todo'],
+//   template: '<li>{{ todo.text }}</li>'
+// }
+//
+// const TodoList = {
+//   data() {
+//     return {
+//       groceryList: [
+//         { id: 0, text: 'Vegetables' },
+//         { id: 1, text: 'Cheese' },
+//         { id: 2, text: 'Whatever else humans are supposed to eat' }
+//       ]
+//     }
+//   },
+//   components: {
+//     TodoItem
+//   }
+// }
+//
+//
+// const app = Vue.createApp(TodoList)
+//
+// app.mount('#todo-list-app')
+//
+//
+// Vue.createApp(EventHandling).mount('#event-handling')
+// Vue.createApp(TwoWayBinding).mount('#two-way-binding')
+// Vue.createApp(ConditionalRendering).mount('#conditional-rendering')
+// Vue.createApp(ListRendering).mount('#list-rendering')
