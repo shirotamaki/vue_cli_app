e874b67a185d29bcaabef7635205057a15138f78
shirotamaki <shiro.tamaki63@gmail.com>
2022-06-23 23:11:28 +0900

    Add: コンポーネントを追加

--- vue_cli_app/src/App.vue
+++ vue_cli_app/src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <HelloWorld msg="Hello FBC"/>
+  <div id="app">
+    <HeaderTitle msg="メモアプリ"/>
+    <MemoList />
+    <AddMemo />
+    <EditDelete />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderTitle from "./components/HeaderTitle.vue";
+import MemoList from "./components/MemoList.vue";
+import AddMemo from "./components/AddMemo.vue";
+import EditDelete from "./components/EditDelete.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    HeaderTitle,
+    MemoList,
+    AddMemo,
+    EditDelete,
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
